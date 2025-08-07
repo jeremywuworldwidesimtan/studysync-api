f563691b88772025d4379caa5f883a9acffb154b
Law Teck Qin <lawteckqin@gmail.com>
2025-08-07 18:33:47 +0800

    Update Cors issue

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ const env = require("dotenv");
 const routes = require("./routes");
 const generative = require("./generativeRoutes");
 //const test = require("./testRoutes");
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api", routes);
 app.use("/api/generative", generative);
 //app.use('/api/test', test);
 
-const PORT = env.config().PORT || 3420;
+env.config(); // just load the .env file
+
+const PORT = process.env.PORT || 3420;
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
